39eb47a634a6a5c8f15e236e8937f2d5967492fd
May Ng <mngmay@gmail.com>
2019-07-22 12:35:32 -0700

    Added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/:id" component={Movie} />
     </div>
   );
 };
