7de5e2a24495bb0b07220fe96e80799fb9dd4f5f
May Ng <mngmay@gmail.com>
2019-07-22 14:31:31 -0700

    Refactored to utilize MovieCard component and replaced old JSX with it in Movie and MovieDetails

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 import axios from "axios";
 
 const Movie = props => {
@@ -13,12 +14,11 @@ const Movie = props => {
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
         setMovie(response.data);
-        console.log(response.data);
       })
       .catch(error => {
         console.error(error);
       });
-  }, [props]);
+  }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -30,28 +30,7 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  return <MovieCard movie={movie} />;
 };
 
 export default Movie;
