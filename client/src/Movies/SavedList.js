fc4c7901d9712bf69e5e333870f0b697a0bd2566
May Ng <mngmay@gmail.com>
2019-07-23 13:02:43 -0700

    Completed last stretch by turning Saved Movie Links into NavLinks and applying activeClassName=saved-active

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`} key={movie.id}>
+      <NavLink
+        to={`/movies/${movie.id}`}
+        key={movie.id}
+        activeClassName="saved-active"
+      >
         <span className="saved-movie">{movie.title}</span>
-      </Link>
+      </NavLink>
     ))}
     <Link to="/">
       <div className="home-button">Home</div>
