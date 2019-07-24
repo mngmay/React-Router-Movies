9663ce15a588a5b4d742fa78cad80cc2fa8559c2
May Ng <mngmay@gmail.com>
2019-07-23 20:31:11 -0700

    Removed text decoration from links

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -36,19 +36,9 @@ const MovieCard = props => {
       <div className="save-wrapper">
         <div className="movie-card">
           <h2>{title}</h2>
-          {/* <div className="movie-director">
-            Director: <em>{director}</em>
-          </div> */}
           <div className="movie-metascore">
             Metascore: <strong>{metascore}</strong>
           </div>
-          {/* <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))} */}
         </div>
       </div>
     );
